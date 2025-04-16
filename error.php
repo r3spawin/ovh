36ef45a10e77598167fdb49579f1fe8a998fae0b
r3spawin <85081037+r3spawin@users.noreply.github.com>
2025-04-16 16:05:17 +0100

    Update error.php

--- error.php
+++ error.php
@@ -64,8 +64,8 @@ $root_path = $_SERVER['DOCUMENT_ROOT'];
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
-$root_url = '';
-
+$root_url = '/var/www/';
+ 
 // Server hostname. Can set manually if wrong
 // $_SERVER['HTTP_HOST'].'/folder'
 $http_host = $_SERVER['HTTP_HOST'];
